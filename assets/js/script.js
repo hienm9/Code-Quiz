0001faa1293e8fa4f47d469054100361de3d7c02
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-03-05 12:37:03 -0500

    update the style for the quiz section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-let startBtn = document.querySelector('#tartQuiz');
+let startBtn = document.querySelector('#start-quiz');
 let timeEL = document.querySelector('#time');
 
 let quizHeader = document.querySelector('quiz-header');
 let choicesList = document.querySelector("choices-list");
-let quizInitial = document.querySelector("quiz-initial");
 let timeCount = document.getElementById("time-count")
 
 let score = 0;
@@ -17,10 +16,31 @@ let holdInterval = 0;
 // subtract time
 let penalty = 15;
 
-// create a listener when click on the start quiz
-startBtn.addEventListener('click',countDown);
+// create a listener for the start button and call startQuiz function
+startBtn.addEventListener('click',startQuiz);
 
+function startQuiz() {
+    // hide the default displays
+    quizInitial.setAttribute("style", "display: none");
+
+}
 function countDown() {
 timer.addEventListener("click")
 }
 
+//Click on the Start Quiz button, it first starts the timer
+
+//Timer starts with 75 seconds, then counts down every second
+
+// For every question, when user selects an answer, 
+// call the compare the function. 
+// if answer is right, display text Correct
+// if answer is wrong, display text Wrong, subtract 10 seconds 
+
+// When all questions are answered:
+// display the final score
+// display the initials box
+// display the Submit button
+// display the text: End quiz
+
+// 
