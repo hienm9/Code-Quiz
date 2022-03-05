738d274a7dc1cb27ac9e7bca6032852682ef1c37
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-03-04 22:43:33 -0500

    add timer to js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let startBtn = document.querySelector('#tartQuiz');
+let timeEL = document.querySelector('#time');
+
+// initial seconds is 75
+let secondInit = 75;
+// Holds interval time
+let holdInterval = 0;
+// subtract time
+let penalty = 15;
+
+// create a listener when click on the start quiz
+startBtn.addEventListener('click',countDown);
+
+function countDown() {
+
+}
+
